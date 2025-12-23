abfaa5ec4cdbb33b8e2f5017805dad510bcf8e37
Hirek <polomarket73@gmail.com>
2025-12-23 08:53:50 +0100

    Move Scenarios view to separate file

---
+++ StarterNG/Views/Scenarios.axaml.cs
@@ -0,0 +1,13 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace StarterNG.Views;
+
+public partial class Scenarios : UserControl
+{
+    public Scenarios()
+    {
+        InitializeComponent();
+    }
+}
